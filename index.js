e906c04a2530e89574ba0d3f068574191976f28f
light0617 <light11470617@gmail.com>
2018-08-09 22:38:53 -0700

    Express Router dish ok

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.use((req, res, next) =>{
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-});
+app.use('/dishes', dishRouter);
+
+
+// app.use((req, res, next) =>{
+//   console.log(req.headers);
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+// });
 
 const server = http.createServer(app);
 
