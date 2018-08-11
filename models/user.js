b819ad3514e9bde35c4e5d064fe891a2668aaeca
light0617 <light11470617@gmail.com>
2018-08-11 01:39:11 -0700

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -4,28 +4,21 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
     admin:   {
         type: Boolean,
         default: false
     }
 });
 
+
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
-
-// var User = new Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password:  {
-//         type: String,
-//         required: true
-//     },
-//     admin:   {
-//         type: Boolean,
-//         default: false
-//     }
-// });
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -15,6 +15,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
 .get((req,res,next) => {
     Dishes.find({})
+    .populate('comments.author')
     .then((dishes) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -49,6 +50,7 @@ dishRouter.route('/')
 dishRouter.route('/:dishId')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
+    .populate('comments.author')
     .then((dish) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -84,6 +86,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
+    .populate('comments.author')
     .then((dish) => {
         if (dish != null) {
             res.statusCode = 200;
@@ -102,6 +105,7 @@ dishRouter.route('/:dishId/comments')
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null) {
+            req.body.author = req.user._id;
             dish.comments.push(req.body);
             dish.save()
             .then((dish) => {
@@ -149,6 +153,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
+    .populate('comments.author')
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
             res.statusCode = 200;
