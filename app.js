2dfb787a11140541ff1b718d55220ba1a84bd8cc
light0617 <light11470617@gmail.com>
2018-08-11 01:23:32 -0700

    Passport JWT

--- app.js
+++ app.js
@@ -13,9 +13,16 @@ var feedbackRouter = require('./routes/feedbackRouter');
 
 const mongoose = require('mongoose');
 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 var app = express();
 
-const url = 'mongodb://localhost:27017/conFusion';
+var config = require('./config');
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -32,8 +39,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser('12345-67890-09876-54321'));
 
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(session({
   name: 'session-id',
@@ -43,29 +50,27 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
     }
     else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+          next();
     }
-  }
 }
+
 
 
 app.use(auth);
