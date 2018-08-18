3134294ab52c6bc5b704094716cf84b34050f0fa
light0617 <light11470617@gmail.com>
2018-08-17 19:29:44 -0700

    ok

--- app.js
+++ app.js
@@ -47,19 +47,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -69,7 +56,6 @@ app.use('/users', usersRouter);
 
 function auth (req, res, next) {
     console.log(req.user);
-
     if (!req.user) {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
