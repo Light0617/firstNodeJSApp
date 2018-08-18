3393e573208fbb5c5c002bee2b87dc17668edb29
light0617 <light11470617@gmail.com>
2018-08-18 14:37:25 -0700

    add

--- authenticate.js
+++ authenticate.js
@@ -8,6 +8,7 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 var config = require('./config.js');
 
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -41,15 +42,33 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
 exports.verifyAdmin = (req, res, next) => {
-    console.log(req.user.admin);
-        if(!req.user.admin)
-        {
-            err = new Error ('not admin');
-            err.status = 403;
-            throw err;
-        }
-        else
-        {
-            next();
-        }
-    };
+  console.log(req.user.admin);
+      if(!req.user.admin){
+          err = new Error ('not admin');
+          err.status = 403;
+          throw err;
+      }else{
+          next();
+      }
+};
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+  clientID: config.facebook.clientId,
+  clientSecret: config.facebook.clientSecret},
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+      if(err) return done(err, false);
+      if(!err && user != null) return done(null, user);
+      else{
+        user = new User({username : profile.displayName});
+        user.facebookId = profile.id;
+        user.firstname = profile.name.givenName;
+        user.lastname = profile.name.familyName;
+        user.save((err, user) => {
+          if(err) return done(err, false);
+          else return done(null, user);
+        })
+      }
+    });
+  }
+));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion2'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion2',
+    'facebook': {
+        clientId: '2138626112878076',
+        clientSecret: '38ddd6f5755ea4db1f695392f05e5574'
+    }
 }
